1622bba7f476e08b7a1d1103478712da35c8111d
zakeverywhere <zacc2010@live.com>
2016-12-01 19:22:51 +1100

    task 19 code clean

--- taskNineteen/task.js
+++ taskNineteen/task.js
@@ -1,110 +1,132 @@
 window.onload = function(){
-  var stack = [];
-  updateDisplay();
-  var timerOutter;
-  var sortFlag = false;
-  $("left-in").addEventListener("click",function(event){
-   if(sortFlag){alert("im sorting!!")}
-    if(getInput()){
-      stack.splice(0,0,getInput());
-      updateDisplay();
-    }
-  })
+   var stack = [10,20,30,50,30,20,10,80,20];
+   var timer;
+   var delay = 50;
 
-  $("right-in").addEventListener("click",function(event){
-   if(sortFlag){alert("im sorting!!")}
-    if(getInput()){
-      stack.push(getInput());
-      updateDisplay();
-    }
-  })
+   //helper function to get element from id
+   function $(id){
+      return document.getElementById(id);
+   }
 
-  $("left-out").addEventListener("click",function(event){
-   if(sortFlag){alert("im sorting!!")}
-    if(stack.length === 0 ){return}
-    var value = stack.splice(0,1)[0];
-    
-    updateDisplay();
-  })
+   //helper function to find is stack sorted
+   function isSorted(){
+      for (var i = 0; i < stack.length-1; i++) {
+      if(stack[i+1]<stack[i]){return false;}
+      }
+      return true;
+   }
 
-  $("right-out").addEventListener("click",function(event){
-   if(sortFlag){alert("im sorting!!")}
-    if(stack.length === 0 ){return}
-    var value = stack.pop();
-    updateDisplay();
-  })
+   //get input from user
+   function getInput(){
+      var input = $("input").value.trim();
+      //check input value.
+      if(input === "") {return false;}
+      if(!(input>=10 && input<=100)) {alert("input has to be between 10 to 100");return false;}
+      if(stack.length>=60){alert("there are 60 elements already!");return false}
+      return input;
+   }
 
+   //color comparing pair in sorting
+   function colorSelected(i,j){
+      var bars = document.getElementsByClassName("bar");
+      bars[i].style.backgroundColor = "pink";
+      bars[j].style.backgroundColor = "pink";
+   }
 
-   $("sort").addEventListener("click",function(event){
-      if(sortFlag){alert("im sorting!!")}
-      if(sortFlag){return;}
-      sortFlag = true;
+   //helper to compare 2 numbers in stack by their index
+   function compare(i,j){
+      if(stack[i]>stack[j]){
+         var temp = stack[i];
+         stack[i] = stack[j];
+         stack[j] = temp;
+      }
+   } 
+
+   //turn on and off buttons
+   function disableBtns(disabled){
+      var btns = document.getElementsByTagName("button");
+      for (var i = 0; i < btns.length; i++) {
+         btns[i].disabled = disabled;
+      }
+   }
+   //function for sort
+   function bubbleSort(){
+      //disable buttons
+      disableBtns(true);
 
       if(stack.length < 2 ){return}
-      var i = 0;
-      var j = stack.length-2;
-      var timerOutter = window.setInterval(function(){
-         updateDisplay();
+
+      //commit sorting
+      var i=0,j = stack.length-1;
+      timer = window.setInterval(function(){
+         renderStack();
          if(!isSorted()){
-            
-            colorSelected(i);
+            //compare 2 numbers
+            colorSelected(i,i+1);
+            compare(i,i+1);
 
-            if(i<j){
-               i++;
-            }else{
-               j--;
-               i=0;
+            if(i===j-1){
+               i=0;j--;return;
             }
+            i++;
          }else{
-            clearTimeout(timerOutter);
-            sortFlag = false;
-            alert("sort finished!");
+            clearTimeout(timer);
+            disableBtns(false);
          }
-      },500);
-   })
-
-  function isSorted(){
-   for (var i = 0; i < stack.length-1; i++) {
-      if(stack[i+1]<stack[i]){return false;}
+      },delay);
    }
-   return true;
-  }
 
-  function colorSelected(i){
-      var bars = document.getElementsByClassName("bar");
-      bars[i].className += " selected";
-      bars[i+1].className += " selected";
-      if(stack[i+1]<stack[i]){
-         //swap number
-         var temp = stack[i];
-         stack[i] = stack[i+1];
-         stack[i+1] = temp;
+   //render stack using div
+   function renderStack(){
+      $("display").innerHTML = "";
+      for (var i = 0; i < stack.length; i++) {
+         var bar = document.createElement('div');
+         bar.className = "bar";
+         bar.innerHTML = stack[i];
+         bar.style.height = stack[i];
+         bar.style.left = i*32;//32 = 30px(width)+2px(padding);
+         $("display").appendChild(bar);
       }
    }
 
-  function updateDisplay(){
+   
 
-    $("display").innerHTML = "";
-    for (var i = 0; i < stack.length; i++) {
-      var bar = document.createElement('div');
-      bar.className = "bar";
-      bar.innerHTML = stack[i];
-      bar.style.height = stack[i];
-      bar.style.left = i*32;
-      bar.animate({height:'100'});
-      $("display").appendChild(bar);
-    }
-  }
+   //functions for button handler
+   function leftIn(){
+      if(getInput()){
+         stack.splice(0,0,getInput());
+         renderStack();
+      }
+   }
 
-  function getInput(){
-    var input = $("input").value;
-    //check input value.
-    if(!(input>=10 && input<=100)) {alert("false input");return false;}
-    if(stack.length>=60){alert("there are 60 elements already!");return false}
-    return input;
-  }
+   function leftOut(){
+      if(stack.length === 0 ){return}
+      stack.splice(0,1);
+      renderStack();
+   }
 
-  function $(id){
-    return document.getElementById(id);
-  }
+   function rightIn(){
+      if(getInput()){
+         stack.push(getInput());
+         renderStack();
+      }
+   }
+
+   function rightOut(){
+      if(stack.length === 0 ){return}
+      stack.pop();
+      renderStack();
+   }
+
+   function initHandler(){
+      $("left-in").addEventListener("click",leftIn);
+      $("left-out").addEventListener("click",leftOut);
+      $("right-in").addEventListener("click",rightIn);
+      $("right-out").addEventListener("click",rightOut);
+      $("sort").addEventListener("click",bubbleSort);
+   }
+
+   initHandler();
+   renderStack();
+
 }
